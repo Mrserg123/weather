77fffbd2692e1fc80edb761f44830bdaaf37f515
Mrserg123 <perpelov@mail.ru>
2022-06-29 21:32:03 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import "./App.css";
 import * as React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import City from "./components/City/City";
 
 function App() {
   return (
     <>
-      <HashRouter>
-        {/* <BrowserRouter> */}
+      <BrowserRouter>
         <Routes>
           <Route index path="/" element={<Home />} />
           <Route path="/weather/:city" element={<City />} />
         </Routes>
-        {/* </BrowserRouter> */}
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
